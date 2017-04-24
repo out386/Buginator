745dc352556d0d77a6f84366319f4904c2721760
Brian C <brian.m.carlson@gmail.com>
2017-04-24 18:16:18 +0530

    Fix pool dp

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 const pg = require('pg');
-var config = {
-  host: process.env.DATABASE_URL
+const url = require('url')
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1]
 };
 
 const pool = new pg.Pool(config);
